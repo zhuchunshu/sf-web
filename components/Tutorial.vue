d41bbe9bae9d7aef10e0e36b1a856d14b9eca5d0
zhuchunshu <laravel@88.com>
2021-07-14 22:43:19 +0800

    Init Commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,7 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <button class="btn btn-primary">DaisyUI Button</button>
+  </div>
+</template>
